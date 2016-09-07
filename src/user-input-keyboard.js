98c72cf89f654879bde6dd9510ec784b56dabebc
cjacobs <apexearth@gmail.com>
2016-09-07 14:45:37 -0700

    Add .clear() to clear all values back to zero.

--- src/user-input-keyboard.js
+++ src/user-input-keyboard.js
@@ -1,10 +1,10 @@
-var util      = require("util")
-var keyboard  = require("key-events")
+var util = require("util")
+var keyboard = require("key-events")
 var EventEmitter = require("events").EventEmitter;
 
 var browser = require("./browser.js")
 
-module.exports               = keyboardInput
+module.exports = keyboardInput
 browser.window.keyboardInput = keyboardInput
 
 function keyboardInput(target) {
@@ -13,14 +13,21 @@ function keyboardInput(target) {
 
 function KeyboardInput(input) {
     var that = this;
+    var keys = [];
     EventEmitter.call(this, input)
     this._input = input
 
     this._input.on('keydown', function (key) {
+        if (keys.indexOf(key) === -1) keys.push(key);
         that[key] = 1;
     })
     this._input.on('keyup', function (key) {
         that[key] = 0;
     })
+    this.clear = function () {
+        for (var i = 0; i < keys.length; i++) {
+            that[keys[i]] = 0;
+        }
+    }
 }
 util.inherits(KeyboardInput, EventEmitter)
--- test/user-input-keyboard-Spec.js
+++ test/user-input-keyboard-Spec.js
@@ -21,4 +21,15 @@ describe("user-input-keyboard.js", function () {
         expect(keyboard[key]).toEqual(0)
     })
 
+    it("can clear all values back to zero", function () {
+        var keyboard = keyboardInput()
+        keyboard._input.emit('keydown', 'A')
+        keyboard._input.emit('keydown', 'B')
+        expect(keyboard['A']).toEqual(1)
+        expect(keyboard['B']).toEqual(1)
+        keyboard.clear();
+        expect(keyboard['A']).toEqual(0)
+        expect(keyboard['B']).toEqual(0)
+    })
+
 })
